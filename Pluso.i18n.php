51179010863660fd0a3d4ff3c6e220c9378f02c6
Alexandr <admin@wikimultia.org>
2015-03-18 20:47:53 +0300

    Update Pluso.i18n.php

--- Pluso.i18n.php
+++ Pluso.i18n.php
@@ -7,14 +7,18 @@
 
 $messages = array();
 
-/** Russian
- * @author Alexandr Efremov
- */
+/** Ukrainian **/
+$messages['uk'] = array(
+	'pluso-desc'		=> 'Додає [http://www.pluso.ru соціальні кнопки] в ліве меню і заголовок сторінки.',
+	'pluso'			=> 'Поділитися',
+);
+/** Russian **/
 $messages['ru'] = array(
 	'pluso-desc'		=> 'Добавляет [http://www.pluso.ru социальные кнопки] в левое меню и заголовок страницы.',
-	'pluso'				=> 'Поделиться',
+	'pluso'			=> 'Поделиться',
 );
+/** English **/
 $messages['en'] = array(
 	'pluso-desc'		=> 'Adds [http://www.pluso.ru social buttons] in the left menu and the page title.',
-	'pluso'				=> 'Share',
+	'pluso'			=> 'Share',
 );
