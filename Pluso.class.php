074ed4071bbfe97fe5df4e94b84f8ff4121cdb0b
Alexandr <admin@wikimultia.org>
2017-09-10 05:32:08 +0300

    Update Pluso.class.php
    Added the ability to change options, such as the shape of an icon, etc.

--- Pluso.class.php
+++ Pluso.class.php
@@ -8,7 +8,7 @@
 class Pluso {
 	/* Function for article header toolbar */
 	public static function PlusoHeader( &$article, &$outputDone, &$pcache ) {
-		global $wgOut, $wgPlusoUserID, $wgPlusoHeader, $wgPlusoMain, $wgPlusoBackgroundHeader, $wgPlusoThemeHeader, $wgPlusoServicesHeader;
+		global $wgOut, $wgPlusoUserID, $wgPlusoHeader, $wgPlusoMain, $wgPlusoBackgroundHeader, $wgPlusoThemeHeader, $wgPlusoServicesHeader, $wgPlusoOptionsHeader;
 		
 		# Check if page is in content namespace and the setting to enable/disable article header tooblar either on the main page or at all
 		if ( !MWNamespace::isContent( $article->getTitle()->getNamespace() )
@@ -17,27 +17,27 @@ class Pluso {
 		) {
 			return true;
 		}
-		
 		$wgOut->setIndicators([
-			'тест'=>'<div class="pluso" data-user="'.$wgPlusoUserID.'" data-background="'.$wgPlusoBackgroundHeader.'" data-options="small,square,line,horizontal,counter,theme='.$wgPlusoThemeHeader.'" data-services="'.$wgPlusoServicesHeader.'"></div>'
+			'pluso'=>'<div class="pluso" data-user="'.$wgPlusoUserID.'" data-background="'.$wgPlusoBackgroundHeader.'" data-options="'.implode(",", $wgPlusoOptionsHeader).',theme='.$wgPlusoThemeHeader.'" data-services="'.$wgPlusoServicesHeader.'"></div>'
 		]);
-		$wgOut->addHTML('<script type="text/javascript">(function() {
+		$wgOut->addHTML('
+			<script type="text/javascript">(function() {
 				if (window.pluso)if (typeof window.pluso.start == "function") return;
 				if (window.ifpluso==undefined) { window.ifpluso = 1;
-					var d = document, s = d.createElement("script"), g = "getElementsByTagName";
-						s.type = "text/javascript"; s.charset="UTF-8"; s.async = true;
-						s.src = ("https:" == window.location.protocol ? "https" : "http")  + "://share.pluso.ru/pluso-like.js";
-					var h=d[g]("body")[0];
-						h.appendChild(s);
-				}
-			})();</script>');
+					var d = document, s = d.createElement(\'script\'), g = \'getElementsByTagName\';
+					s.type = \'text/javascript\'; s.charset=\'UTF-8\'; s.async = true;
+					s.src = (\'https:\' == window.location.protocol ? \'https\' : \'http\')  + \'://share.pluso.ru/pluso-like.js\';
+					var h=d[g](\'body\')[0];
+					h.appendChild(s);
+			}})();</script>
+		');
 		
 		return true;
 	}
 
 	/* Function for sidebar*/
 	public static function PlusoSidebar( $skin, &$bar ) {
-		global $wgOut, $wgPlusoUserID, $wgPlusoSidebar, $wgPlusoBackgroundSidebar, $wgPlusoThemeSidebar, $wgPlusoServicesSidebar;
+		global $wgOut, $wgPlusoUserID, $wgPlusoSidebar, $wgPlusoBackgroundSidebar, $wgPlusoThemeSidebar, $wgPlusoServicesSidebar,$wgPlusoOptionsSidebar;
 
 		# Load css stylesheet
 		$wgOut->addModuleStyles( 'ext.plusohis' );
@@ -58,7 +58,7 @@ class Pluso {
 					var h=d[g](\'body\')[0];
 					h.appendChild(s);
 			}})();</script>
-			<div data-user="'.$wgPlusoUserID.'" class="pluso" data-background="'.$wgPlusoBackgroundSidebar.'" data-options="small,square,multiline,horizontal,nocounter,theme='.$wgPlusoThemeSidebar.'" data-services="'.$wgPlusoServicesSidebar.'"></div>';
+			<div data-user="'.$wgPlusoUserID.'" class="pluso" data-background="'.$wgPlusoBackgroundSidebar.'" data-options="'.implode(",", $wgPlusoOptionsSidebar).',theme='.$wgPlusoThemeSidebar.'" data-services="'.$wgPlusoServicesSidebar.'"></div>';
 		return true;
 	}
 }
