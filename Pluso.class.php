5115e3670e7ba4ab9b6172f8d4813d580ea49c5b
Alexandr <admin@wikimultia.org>
2021-09-11 15:21:14 +0300

    Update Pluso.class.php

--- Pluso.class.php
+++ Pluso.class.php
@@ -6,6 +6,11 @@
  * @ingroup Extensions
  */
 class Pluso {
+    public static function onBeforePageDisplay(OutputPage $out, Skin $skin) {
+        // Some code...
+
+        $out->addModules('ext.Pluso');
+    }
 	/* Function for article header toolbar */
 	public static function onArticleViewHeader( &$article, &$outputDone, &$pcache ) {
 		global $wgOut, $wgPlusoUserID, $wgPlusoHeader, $wgPlusoMain, $wgPlusoBackgroundHeader, $wgPlusoThemeHeader, $wgPlusoServicesHeader, $wgPlusoOptionsHeader;
@@ -18,37 +23,26 @@ class Pluso {
 			return true;
 		}
 		
-		$wgOut->setIndicators(['pluso'=>'<div class="pluso" data-user="'.$wgPlusoUserID.'" data-background="'.$wgPlusoBackgroundHeader.'" data-options="'.implode(",", $wgPlusoOptionsHeader).',theme='.$wgPlusoThemeHeader.'" data-services="'.$wgPlusoServicesHeader.'"></div>']);
+		$wgOut->setIndicators(['pluso'=>'<div class="pluso" data-background="'.$wgPlusoBackgroundHeader.'" data-options="'.implode(",", $wgPlusoOptionsHeader).',theme='.$wgPlusoThemeHeader.'" data-services="'.$wgPlusoServicesHeader.'"></div>']);
 	}
 
 	/* Function for sidebar*/
 	public static function onSkinBuildSidebar( $skin, &$bar ) {
 		global $wgOut, $wgPlusoUserID, $wgPlusoSidebar, $wgPlusoBackgroundSidebar, $wgPlusoThemeSidebar, $wgPlusoServicesSidebar,$wgPlusoOptionsSidebar;
-		$wgOut->addScript('<script type="text/javascript">(function() {
-	  if (window.pluso)if (typeof window.pluso.start == "function") return;
-	  if (window.ifpluso==undefined) { window.ifpluso = 1;
-		var d = document, s = d.createElement(\'script\'), g = \'getElementsByTagName\';
-		s.type = \'text/javascript\'; s.charset=\'UTF-8\'; s.async = true;
-		s.src = (\'https:\' == window.location.protocol ? \'https\' : \'http\')  + \'://share.pluso.ru/pluso-like.js\';
-		var h=d[g](\'body\')[0];
-		h.appendChild(s);
-	  }})();</script>');
 		
 		# Check setting to enable/disable sidebar portlet
 		if ( !$wgPlusoSidebar ) {
 			return true;
 		}
-		
 		$bar['pluso'][] = [
-			'text'  			=> 'pluso',
-			'href'  			=> '#',
-			'title' 			=> 'pluso',
-			'id'    			=> 'pluso',
-			'link-class'		=> 'pluso',
-			'data-background'	=> $wgPlusoBackgroundSidebar,
-			'data-options'		=> implode(",", $wgPlusoOptionsSidebar).',theme='.$wgPlusoThemeSidebar,
-			'data-services' 	=> $wgPlusoServicesSidebar,
-			'data-user'			=> $wgPlusoUserID
+			'text'            => 'pluso',
+			'href'            => '#',
+			'title'           => 'pluso',
+			'id'              => 'pluso',
+			'link-class'      => 'pluso',
+			'data-background' => $wgPlusoBackgroundSidebar,
+			'data-options'    => implode(",", $wgPlusoOptionsSidebar).',theme='.$wgPlusoThemeSidebar,
+			'data-services'   => $wgPlusoServicesSidebar
 		];
 	}
 }
